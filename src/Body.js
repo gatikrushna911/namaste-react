aa8bcd4a6ce039f868cda504a1d107e1cfd95fd7
gatikrushna911 <gatikrushna911@gmail.com>
2023-03-06 07:34:49 +0530

    Passing props into Restaurant card

--- src/Body.js
+++ src/Body.js
@@ -1,9 +1,30 @@
+import { IMG_URL_CDN, restaurantList } from "./utils/Constants";
+
+const RestaurantCard = (props) => {
+  // const { name, avgRating, cloudinaryImageId } = restaurant;
+  return (
+    <>
+      <div className="restaurant-card">
+        <img src={IMG_URL_CDN + props.restaurant.cloudinaryImageId}></img>
+        <h1>{props.restaurant.name}</h1>
+        <h4>Ratings {props.restaurant.avgRating} stars</h4>
+      </div>
+    </>
+  );
+};
 const Body = () => {
   return (
     <>
       <div className="search-container">
         <input type="search" className="search-input" placeholder="search" />
         <button className="search-btn">Search</button>
+        {/* <RestaurantCard
+          name={restaurantList[0].data.name}
+          avgRating={restaurantList[0].data.avgRating}
+          cloudinaryImageId={restaurantList[0].data.cloudinaryImageId}
+        /> */}
+
+        <RestaurantCard restaurant={restaurantList[0].data} />
       </div>
     </>
   );
